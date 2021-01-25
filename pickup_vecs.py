a0120b385b3f4e7ab2994d1e6fdf84020e0d4c04
Yusuke Matsui <matsui528@gmail.com>
2021-01-25 11:43:09 +0900

    delete pip dependency, revised a readme

--- pickup_vecs.py
+++ pickup_vecs.py
@@ -1,9 +1,10 @@
 # Some IO codes are from:
 # - https://github.com/facebookresearch/faiss/blob/master/contrib/vecs_io.py
+# - https://github.com/Hi-king/read_texmex_dataset_python/blob/master/texmex_python/reader.py
 
 import numpy as np
 import argparse
-import texmex_python
+import struct
 
 def process_args():
     parser = argparse.ArgumentParser()
@@ -27,13 +28,24 @@ def fvecs_write(fname, m):
 if __name__ == "__main__":
     args = process_args()
 
-    # Reat topk vector one by one
-    vecs = []
+    # Read topk vector one by one
+    vecs = []    
     with open(args.src, "rb") as f:
-        for vec in texmex_python.reader.read_fvec_iter(f):
+        while 1:
+            # The first 4 byte is for the dimensionality
+            dim_bin = f.read(4)
+            if dim_bin == b'':
+                break
+
+            # The next 4 * dim byte is for a vector
+            dim, = struct.unpack('i', dim_bin)
+            vec = struct.unpack('f' * dim, f.read(4 * dim))
+            
+            # Store it
             vecs.append(vec)
             if len(vecs) == args.topk:
                 break
+            
     vecs = np.array(vecs, dtype=np.float32)
     assert vecs.shape[0] == args.topk
     print("vecs.shape:", vecs.shape)
